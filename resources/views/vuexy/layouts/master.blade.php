906f0b8cdddc7dd2cfe2a94658b9c5d0d1f78e07
dennohdee <denniskiprotich0@gmail.com>
2021-05-16 17:54:25 +0300

    Assest bundling

--- resources/views/vuexy/layouts/master.blade.php
+++ resources/views/vuexy/layouts/master.blade.php
@@ -36,32 +36,32 @@
     <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
     <meta name="author" content="PIXINVENT">
     <title> @yield('title') - {{ config('app.name')}}</title>
-    <link rel="apple-touch-icon" href="../tyu/app-assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="../tyu/app-assets/images/ico/favicon-16x16e.png">
+    <link rel="apple-touch-icon" href="{{ asset('tyu/app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('tyu/app-assets/images/ico/favicon-16x16e.png')}}">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
     <!-- BEGIN: Vendor CSS-->
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/vendors/css/vendors.min.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/vendors/css/charts/apexcharts.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/vendors/css/vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/vendors/css/charts/apexcharts.css')}}">
     <!-- END: Vendor CSS-->
 
     <!-- BEGIN: Theme CSS-->
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/bootstrap-extended.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/colors.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/components.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/themes/dark-layout.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/themes/semi-dark-layout.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/themes/semi-dark-layout.css')}}">
 
     <!-- BEGIN: Page CSS-->
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/core/menu/menu-types/vertical-menu.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/core/colors/palette-gradient.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/pages/dashboard-ecommerce.css">
-    <link rel="stylesheet" type="text/css" href="../tyu/app-assets/css/pages/card-analytics.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/core/menu/menu-types/vertical-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/core/colors/palette-gradient.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/pages/dashboard-ecommerce.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/app-assets/css/pages/card-analytics.css')}}">
     <!-- END: Page CSS-->
 
     <!-- BEGIN: Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="../tyu/assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('tyu/assets/css/style.css')}}">
     <!-- END: Custom CSS-->
 
 </head>
