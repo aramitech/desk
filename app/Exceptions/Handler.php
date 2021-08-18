c684d99c2c8fc52b4034021ef91cefc60db6fcc2
Dennis Kiprotich <denniskiprotich0@gmail.com>
2021-08-18 18:09:17 +0300

    Change on handler to fix run artisan

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,52 +1,18 @@
 <?php
 
 namespace App\Exceptions;
-use Illuminate\Auth\AuthenticationException as AuthenticationException;
+
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Throwable;
+use Throwable; // <-- ADD THIS
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
-    public function register()
+    public function report(Throwable $exception) // <-- USE Throwable HERE
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
     }
-
-
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception) // AND HERE
     {
-        if ($exception instanceof AuthenticationException) {
-            return redirect('/');
-        }
         return parent::render($request, $exception);
     }
-
-
-
 }
